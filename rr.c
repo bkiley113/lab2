53ea4336806c7c4a51485a267d089c5770a6a766
Brandon Kiley <b.kiley.113@gmail.com>
2025-02-16 23:19:29 -0800

    reorg

--- rr.c
+++ rr.c
@@ -1,202 +1,3 @@
-// #include <errno.h>
-// #include <fcntl.h>
-// #include <stdbool.h>
-// #include <stdint.h>
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <sys/queue.h>
-// #include <sys/mman.h>
-// #include <sys/stat.h>
-// #include <unistd.h>
-
-// typedef uint32_t u32;
-// typedef int32_t i32;
-
-// struct process {
-//     u32 pid;
-//     u32 arrival_time;
-//     u32 burst_time;
-
-//     TAILQ_ENTRY(process) pointers;
-
-//     /* Additional fields here */
-//     u32 remaining_time;
-//     u32 start_time;
-//     u32 last_executed;
-//     bool has_started;
-//     /* End of "Additional fields here" */
-// };
-
-// TAILQ_HEAD(process_list, process);
-
-// u32 next_int(const char **data, const char *data_end) {
-//     u32 current = 0;
-//     bool started = false;
-//     while (*data != data_end) {
-//         char c = **data;
-//         if (c < 0x30 || c > 0x39) {
-//             if (started) {
-//                 return current;
-//             }
-//         } else {
-//             if (!started) {
-//                 current = (c - 0x30);
-//                 started = true;
-//             } else {
-//                 current *= 10;
-//                 current += (c - 0x30);
-//             }
-//         }
-//         ++(*data);
-//     }
-//     printf("Reached end of file while looking for another integer\n");
-//     exit(EINVAL);
-// }
-
-// u32 next_int_from_c_str(const char *data) {
-//     char c;
-//     u32 i = 0;
-//     u32 current = 0;
-//     bool started = false;
-//     while ((c = data[i++])) {
-//         if (c < 0x30 || c > 0x39) {
-//             exit(EINVAL);
-//         }
-//         if (!started) {
-//             current = (c - 0x30);
-//             started = true;
-//         } else {
-//             current *= 10;
-//             current += (c - 0x30);
-//         }
-//     }
-//     return current;
-// }
-
-// void init_processes(const char *path,
-//                     struct process **process_data,
-//                     u32 *process_size) {
-//     int fd = open(path, O_RDONLY);
-//     if (fd == -1) {
-//         int err = errno;
-//         perror("open");
-//         exit(err);
-//     }
-
-//     struct stat st;
-//     if (fstat(fd, &st) == -1) {
-//         int err = errno;
-//         perror("stat");
-//         exit(err);
-//     }
-
-//     u32 size = st.st_size;
-//     const char *data_start = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
-//     if (data_start == MAP_FAILED) {
-//         int err = errno;
-//         perror("mmap");
-//         exit(err);
-//     }
-
-//     const char *data_end = data_start + size;
-//     const char *data = data_start;
-
-//     *process_size = next_int(&data, data_end);
-
-//     *process_data = calloc(sizeof(struct process), *process_size);
-//     if (*process_data == NULL) {
-//         int err = errno;
-//         perror("calloc");
-//         exit(err);
-//     }
-
-//     for (u32 i = 0; i < *process_size; ++i) {
-//         (*process_data)[i].pid = next_int(&data, data_end);
-//         (*process_data)[i].arrival_time = next_int(&data, data_end);
-//         (*process_data)[i].burst_time = next_int(&data, data_end);
-//     }
-
-//     munmap((void *)data, size);
-//     close(fd);
-// }
-
-// int main(int argc, char *argv[]) {
-//     if (argc != 3) {
-//         return EINVAL;
-//     }
-//     struct process *data;
-//     u32 size;
-//     init_processes(argv[1], &data, &size);
-
-//     u32 quantum_length = next_int_from_c_str(argv[2]);
-
-//     struct process_list ready_queue;
-//     TAILQ_INIT(&ready_queue);
-
-//     u32 total_waiting_time = 0;
-//     u32 total_response_time = 0;
-//     u32 current_time = 0;
-//     u32 time_slice = 0;
-
-//     /* Populate the ready queue exactly once for each process */
-//     for (u32 i = 0; i < size; i++) {
-//         data[i].remaining_time = data[i].burst_time;
-//         data[i].has_started = false;
-//         TAILQ_INSERT_TAIL(&ready_queue, &data[i], pointers);
-//     }
-
-//     struct process *curr_proc;
-
-//     while (!TAILQ_EMPTY(&ready_queue)) {
-//         curr_proc = TAILQ_FIRST(&ready_queue);
-//         TAILQ_REMOVE(&ready_queue, curr_proc, pointers);
-
-//         // Fast-forward time if process hasn't arrived yet.
-//         if (curr_proc->arrival_time > current_time) {
-//             current_time = curr_proc->arrival_time;
-//         }
-
-//         // If this is the first time the process runs, record response time.
-//         if (!curr_proc->has_started) {
-//             curr_proc->has_started = true;
-//             total_response_time += (current_time - curr_proc->arrival_time);
-//             curr_proc->start_time = current_time;
-//         }
-
-//         // Determine the time slice to run.
-//         if (curr_proc->remaining_time > quantum_length) {
-//             time_slice = quantum_length;
-//         } else {
-//             time_slice = curr_proc->remaining_time;
-//         }
-
-//         // "Run" the process.
-//         curr_proc->remaining_time -= time_slice;
-//         current_time += time_slice;
-
-//         // If process is not finished, reinsert it at the tail.
-//         // if (curr_proc->remaining_time > 0) {
-//         //     TAILQ_INSERT_TAIL(&ready_queue, curr_proc, pointers);
-//         // } else {
-//         //     // Process finished; compute waiting time.
-//         //     total_waiting_time += (current_time - curr_proc->arrival_time - curr_proc->burst_time);
-//         // }
-
-//         if (curr_proc->remaining_time > 0){
-//           TAILQ_INSERT_TAIL(&ready_queue, curr_proc, pointers);
-//           total_waiting_time += (current_time -curr_proc->last_executed);
-//         }
-//         curr_proc->last_executed = current_time;
-//     }
-
-//     printf("Average waiting time: %.2f\n", (float)total_waiting_time / (float)size);
-//     printf("Average response time: %.2f\n", (float)total_response_time / (float)size);
-
-//     free(data);
-//     return 0;
-// }
-
-
 #include <errno.h>
 #include <fcntl.h>
 #include <stdbool.h>
@@ -216,109 +17,134 @@ struct process {
     u32 arrival_time;
     u32 burst_time;
 
-    // Fields for scheduling
+    TAILQ_ENTRY(process) pointers;
+
+    /* Additional fields here */
+
     u32 remaining_time;
     u32 start_time;       // when it first gets CPU
     u32 completion_time;  // when it finishes
     bool has_started;
-
-    // For incremental waiting calculations (optional)
     u32 last_executed;
 
-    TAILQ_ENTRY(process) pointers;
+    /* End of "Additional fields here" */
 };
 
 TAILQ_HEAD(process_list, process);
 
-u32 next_int(const char **data, const char *data_end) {
-    u32 current = 0;
-    bool started = false;
-    while (*data != data_end) {
-        char c = **data;
-        if (c < '0' || c > '9') {
-            if (started) {
-                return current;
-            }
-        } else {
-            if (!started) {
-                current = (c - '0');
-                started = true;
-            } else {
-                current *= 10;
-                current += (c - '0');
-            }
-        }
-        ++(*data);
+u32 next_int(const char **data, const char *data_end)
+{
+  u32 current = 0;
+  bool started = false;
+  while (*data != data_end)
+  {
+    char c = **data;
+
+    if (c < 0x30 || c > 0x39)
+    {
+      if (started)
+      {
+        return current;
+      }
     }
-    printf("Reached end of file while looking for another integer\n");
-    exit(EINVAL);
+    else
+    {
+      if (!started)
+      {
+        current = (c - 0x30);
+        started = true;
+      }
+      else
+      {
+        current *= 10;
+        current += (c - 0x30);
+      }
+    }
+
+    ++(*data);
+  }
+
+  printf("Reached end of file while looking for another integer\n");
+  exit(EINVAL);
 }
 
-u32 next_int_from_c_str(const char *data) {
-    char c;
-    u32 i = 0;
-    u32 current = 0;
-    bool started = false;
-    while ((c = data[i++])) {
-        if (c < '0' || c > '9') {
-            exit(EINVAL);
-        }
-        if (!started) {
-            current = (c - '0');
-            started = true;
-        } else {
-            current *= 10;
-            current += (c - '0');
-        }
+u32 next_int_from_c_str(const char *data)
+{
+  char c;
+  u32 i = 0;
+  u32 current = 0;
+  bool started = false;
+  while ((c = data[i++]))
+  {
+    if (c < 0x30 || c > 0x39)
+    {
+      exit(EINVAL);
     }
-    return current;
+    if (!started)
+    {
+      current = (c - 0x30);
+      started = true;
+    }
+    else
+    {
+      current *= 10;
+      current += (c - 0x30);
+    }
+  }
+  return current;
 }
 
 void init_processes(const char *path,
                     struct process **process_data,
-                    u32 *process_size) {
-    int fd = open(path, O_RDONLY);
-    if (fd == -1) {
-        int err = errno;
-        perror("open");
-        exit(err);
-    }
+                    u32 *process_size)
+{
+  int fd = open(path, O_RDONLY);
+  if (fd == -1)
+  {
+    int err = errno;
+    perror("open");
+    exit(err);
+  }
 
-    struct stat st;
-    if (fstat(fd, &st) == -1) {
-        int err = errno;
-        perror("stat");
-        exit(err);
-    }
+  struct stat st;
+  if (fstat(fd, &st) == -1)
+  {
+    int err = errno;
+    perror("stat");
+    exit(err);
+  }
 
-    u32 size = st.st_size;
-    const char *data_start = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (data_start == MAP_FAILED) {
-        int err = errno;
-        perror("mmap");
-        exit(err);
-    }
+  u32 size = st.st_size;
+  const char *data_start = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
+  if (data_start == MAP_FAILED)
+  {
+    int err = errno;
+    perror("mmap");
+    exit(err);
+  }
 
-    const char *data_end = data_start + size;
-    const char *data = data_start;
+  const char *data_end = data_start + size;
+  const char *data = data_start;
 
-    *process_size = next_int(&data, data_end);
+  *process_size = next_int(&data, data_end);
 
-    *process_data = calloc(sizeof(struct process), *process_size);
-    if (*process_data == NULL) {
-        int err = errno;
-        perror("calloc");
-        exit(err);
-    }
+  *process_data = calloc(sizeof(struct process), *process_size);
+  if (*process_data == NULL)
+  {
+    int err = errno;
+    perror("calloc");
+    exit(err);
+  }
 
-    for (u32 i = 0; i < *process_size; ++i) {
-        (*process_data)[i].pid = next_int(&data, data_end);
-        (*process_data)[i].arrival_time = next_int(&data, data_end);
-        (*process_data)[i].burst_time = next_int(&data, data_end);
-    }
+  for (u32 i = 0; i < *process_size; ++i)
+  {
+    (*process_data)[i].pid = next_int(&data, data_end);
+    (*process_data)[i].arrival_time = next_int(&data, data_end);
+    (*process_data)[i].burst_time = next_int(&data, data_end);
+  }
 
-    munmap((void *)data_start, size);
-    close(fd);
+  munmap((void *)data, size);
+  close(fd);
 }
 
 /* Comparison function to sort processes by arrival_time ascending */
@@ -331,7 +157,8 @@ static int compare_by_arrival_time(const void *a, const void *b) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
+    if (argc != 3) 
+    {
         return EINVAL;
     }
 
@@ -339,13 +166,16 @@ int main(int argc, char *argv[]) {
     u32 size;
     init_processes(argv[1], &data, &size);
 
-    // Sort the processes by arrival time
-    qsort(data, size, sizeof(struct process), compare_by_arrival_time);
-
     // Initialize
     u32 quantum_length = next_int_from_c_str(argv[2]);
+
     struct process_list ready_queue;
     TAILQ_INIT(&ready_queue);
+
+    /* Your code here*/
+
+    // Sort the processes by arrival time
+    qsort(data, size, sizeof(struct process), compare_by_arrival_time);
 
     // Prepare fields
     for (u32 i = 0; i < size; i++) {
@@ -449,6 +279,8 @@ int main(int argc, char *argv[]) {
     // Print results
     float avg_waiting = total_waiting_time / (float)size;
     float avg_response = total_response_time / (float)size;
+
+    /* End of "Your code here" */
 
     printf("Average waiting time: %.2f\n", avg_waiting);
     printf("Average response time: %.2f\n", avg_response);
